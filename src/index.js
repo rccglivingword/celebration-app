1322c6231bc6cc20b30884a8298fd5b8e903d9fe
pharmlovex <pharmifedayoojo@gmail.com>
2023-10-23 00:52:30 +0100

    Modified index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -11,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+// if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
